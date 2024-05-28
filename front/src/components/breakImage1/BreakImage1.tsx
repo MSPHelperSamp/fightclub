6608bc4844e242e20e1ecd87565dafaf764f2a8f
MSPHelperSamp <MSPHelperSamp>
2024-05-28 19:41:21 +0300

    s

--- front/src/components/breakImage1/BreakImage1.tsx
+++ front/src/components/breakImage1/BreakImage1.tsx
@@ -1,10 +1,9 @@
-import imageBreak from '/imageBreak.svg'
 import './breakImage1.scss'
 
 const BreakImage1 = () => {
 	return (
 		<section className='break'>
-			<img src={imageBreak} alt='image' />
+			{/* <img src={imageBreak} alt='image' /> */}
 		</section>
 	)
 }
